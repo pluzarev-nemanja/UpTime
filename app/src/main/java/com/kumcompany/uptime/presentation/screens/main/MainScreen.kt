600e67926985ed686bf72283670b5e760c91d61d
nemanja <pluzarev.nemanja@gmail.com>
2024-03-17 18:25:38 +0100

    Navigation bug fix

--- app/src/main/java/com/kumcompany/uptime/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/kumcompany/uptime/presentation/screens/main/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,7 +39,7 @@ fun MainScreen(
         NavigationBarItems.Search
     )
 
-    var selectedIndex by remember {
+    var selectedIndex by rememberSaveable {
         mutableStateOf(0)
     }
     val navBackStackEntry by navController.currentBackStackEntryAsState()
